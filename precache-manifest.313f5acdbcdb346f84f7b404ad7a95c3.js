6a53cb9e27f3fc7f287057f0df175e3bf8425913
Sreejish <srijishks@gmail.com>
2019-10-19 17:44:19 +1300

    Updates

---
+++ precache-manifest.313f5acdbcdb346f84f7b404ad7a95c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc596832048af25a3efc70bcdc6aa37",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "e54712e0586bd57a93ab",
+    "url": "/profile/static/css/main.ad90c49e.chunk.css"
+  },
+  {
+    "revision": "f375c85c9c7ef15ac9a4",
+    "url": "/profile/static/js/2.3a17d141.chunk.js"
+  },
+  {
+    "revision": "e54712e0586bd57a93ab",
+    "url": "/profile/static/js/main.fdc6038b.chunk.js"
+  },
+  {
+    "revision": "9d40a29794035599278e",
+    "url": "/profile/static/js/runtime-main.f1010385.js"
+  }
+]);
